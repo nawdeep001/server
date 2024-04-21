3b97140b8003355e6932abe16bd065fe9f320097
Nawdeep <283nawdeep@gmail.com>
2024-04-21 17:26:20 +0530

    new commit

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import { error } from "console";
+import { register } from "./controllers/auth.js";
+
 
 /* CONFIGURATION */
 const __filename = fileURLToPath(import.meta.url);
@@ -35,6 +37,9 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage });
+
+/* Routes with Files */
+app.post("/auth/register", upload.single("picture"), register);
 
 /* Mongo SetUp */
 
